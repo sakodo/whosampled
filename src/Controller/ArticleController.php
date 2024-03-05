4e4fbdda1888ce6729a275479da956659982d375
Khaled199-cell <141149266+Khaled199-cell@users.noreply.github.com>
2024-03-05 16:58:21 +0100

    page Articles

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\Repository\RepositoryFactory;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +13,22 @@ use Symfony\Component\Routing\Attribute\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
-    public function index(): Response
+    public function index(ArticleRepository $article): Response
     {
+        $articles = $article->findAll();
         return $this->render('article/index.html.twig', [
             'controller_name' => 'ArticleController',
+            'articles'=> $articles
         ]);
+        
+    }
+    #[Route('/showarticle', name: 'app_show_article')]
+    public function showarticle(ManagerRegistry $mr): Response
+    {
+        $articles = $mr->getRepository(Article::class)->findAll();
+        return $this->render('article/index.html.twig', [
+            'article' => $articles,
+        ]);
+        
     }
 }
