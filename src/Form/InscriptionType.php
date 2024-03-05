5efbc99a61d7de3f3deba1f7901774acd6f5b69f
dorsainvil-christopher <154424369+dorsainvil-christopher@users.noreply.github.com>
2024-03-05 17:00:58 +0100

    page Hom

--- src/Form/InscriptionType.php
+++ src/Form/InscriptionType.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\User;
+use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,10 +13,12 @@ class InscriptionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-           
+            ->add('firstname')
+            ->add('lastname')
             ->add('email')
             ->add('roles')
             ->add('password')
+            ->add('')
 
         ;
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -20,7 +20,8 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-           
+            ->add('firstname')
+            ->add('lastname')
             ->add('email')
             ->add('agreeTerms', CheckboxType::class, [
                                 'mapped' => false,
@@ -30,7 +31,7 @@ class RegistrationFormType extends AbstractType
                     ]),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('password', PasswordType::class, [
                                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
