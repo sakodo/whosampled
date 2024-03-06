dac739f84ca8d75d2811bc17f6b80efb39366f2b
dorsainvil-christopher <154424369+dorsainvil-christopher@users.noreply.github.com>
2024-03-06 16:59:15 +0100

    page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\AlbumRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Constraints\Length;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(AlbumRepository $albumRepository ): Response
     {
+        $albums = $albumRepository->getDataAlbum();
+        $album = $albums[array_rand($albums)];
+        
+       
+             
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'album' => $album,
         ]);
     }
 
+    
+        
     
 }
