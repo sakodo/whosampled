acad6607bc17bf8d75353c394f72041f65310bda
Khaled199-cell <141149266+Khaled199-cell@users.noreply.github.com>
2024-03-08 16:17:34 +0100

    connexion et déconnxion

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -52,7 +52,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-    return new RedirectResponse($this->urlGenerator->generate('app_article'));
+    return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
     protected function getLoginUrl(Request $request): string
