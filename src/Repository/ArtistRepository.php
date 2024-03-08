672829e263910f5f2c36525d8d9158e54d46df9c
dorsainvil-christopher <154424369+dorsainvil-christopher@users.noreply.github.com>
2024-03-08 15:33:33 +0100

    modification assets dans public

--- src/Repository/ArtistRepository.php
+++ src/Repository/ArtistRepository.php
@@ -32,6 +32,17 @@ class ArtistRepository extends ServiceEntityRepository
            ;
        }
 
+    public function findArtistWithSongsById(int $id): ?Artist
+       {
+           return $this->createQueryBuilder('a')
+               ->leftJoin('a.songs', 's') 
+               ->addSelect('s') 
+               ->andWhere('a.id = :id')
+               ->setParameter('id', $id)
+               ->getQuery()
+               ->getOneOrNullResult();
+       }
+
 
     //    /**
     //     * @return Artist[] Returns an array of Artist objects
