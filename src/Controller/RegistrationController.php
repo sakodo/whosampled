0c62430f25ba8f3059026d462eb858c66de07b41
erika bal <erikabal75@hotmail.com>
2024-03-14 19:42:16 +0100

    commit

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,23 +6,16 @@ use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Security\LoginFormAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, UserAuthenticatorInterface $userAuthenticator, LoginFormAuthenticator $authenticator): Response
-       { 
-        
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
-
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -40,16 +33,11 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $userAuthenticator->authenticateUser(
-                $user,
-                $authenticator,
-                $request
-            );
+            return $this->redirectToRoute('_profiler_home');
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
     }
-
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
-         if ($this->getUser()) {
-             return $this->redirectToRoute('app_home');
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_home');
+        }
+       
+         // Création du formulaire de recherche
+         $form = $this->createForm(SearchType::class);
+         $form->handleRequest($request);
+         
+         // Vérification si le formulaire a été soumis et est valide
+         if($form->isSubmitted() && $form->isValid()) {   
+        
+             // Récupération des données du formulaire
+             $query = $form->getData();
+             // Redirection vers la page de recherche avec les données du formulaire en paramètre
+             return $this->redirectToRoute('app_search', ['query' => $query]);           
+             
          }
+
+        
+        
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -29,4 +49,6 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+      
 }
