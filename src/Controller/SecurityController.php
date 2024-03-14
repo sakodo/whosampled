2a190f60db37697fec287c2d377d6b97d8192c6a
erika bal <erikabal75@hotmail.com>
2024-03-14 20:38:09 +0100

    ameliorations multiples
    condition pour login et register a cause dela barre de recherche
    amelioration de l integration
    ajout des liens entre les pages

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,37 +4,18 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute('app_home');
-        }
-       
-         // Création du formulaire de recherche
-         $form = $this->createForm(SearchType::class);
-         $form->handleRequest($request);
-         
-         // Vérification si le formulaire a été soumis et est valide
-         if($form->isSubmitted() && $form->isValid()) {   
-        
-             // Récupération des données du formulaire
-             $query = $form->getData();
-             // Redirection vers la page de recherche avec les données du formulaire en paramètre
-             return $this->redirectToRoute('app_search', ['query' => $query]);           
-             
+         if ($this->getUser()) {
+             return $this->redirectToRoute('app_home');
          }
-
-        
-        
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -49,6 +30,5 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
-
-      
 }
+
